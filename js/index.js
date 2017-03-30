e2e499be06159b5a1a3af27edebb99a83e0813da
sunhao_win <sunhao@zongheng.com>
2017-03-30 18:08:04 +0800

    0330 worker

--- js/index.js
+++ js/index.js
@@ -61,7 +61,7 @@ $(function (){
     }
     function loadProficient(op){/*高手推荐*/
         var timer=new Date().getTime();
-        var defaultOption={role:3,type:1,page_index:1,page_num:10,t:timer};/*默认北京专场，第一页每页10条*/;
+        var defaultOption={role:2,type:1,page_index:1,page_num:10,t:timer};/*默认北京专场，第一页每页10条*/;
 
         var option=$.extend(defaultOption,op);
         $.ajax({
@@ -72,7 +72,7 @@ $(function (){
             success:function (data){
                 if(data.status==200){
                     var list=data.data;
-                    var recommendhtml='<a href="recommend.html"><dt>高手推荐<span class="more_icon">更多</span></dt></a>';
+                    var recommendhtml='<a href="/recommend.html"><dt>最新推荐<span class="more_icon">更多</span></dt></a>';
                     var typeName=["北京单场","北京单场","胜负玩法","竞彩玩法"];
                     
                     var weekday=["周日","周一","周二","周三","周四","周五","周六"];
@@ -83,7 +83,7 @@ $(function (){
                         var date=new Date(beginTime);
                         var d=date.getDay();
                         if(isBuy==1 || list[i].is_hit){
-                            recommendhtml+='<dd expertid="'+list[i].expertid+'" recommendid="'+list[i].recommendid+'" title="'+list[i].title+'" type="'+type+'"><section class="expert_case"><a class="gotoexpert" expertid="'+list[i].expertid+'" expertimg="'+list[i].imageSrc+'" expertname="'+list[i].nickname+'" expertintro="'+list[i].content+'"><span class="head_cap"><img src="'+list[i].imageSrc+'"></span></a><div class="cap_cont jumpexpert" expertid="'+list[i].expertid+'"><em>'+list[i].nickname+'</em><mark>高级专家</mark></div></section><section class="ball_detail"><div class="ball_time"><small>'+weekday[d]+'</small><p>'+list[i].begin_match_time.substring(5,16)+'</p></div><p class="span_contr"><strong>'+list[i].title+'</strong>';
+                            recommendhtml+='<dd expertid="'+list[i].expertid+'" recommendid="'+list[i].recommendid+'" title="'+list[i].title+'" type="'+type+'" jump="jump"><section class="expert_case"><a class="gotoexpert" expertid="'+list[i].expertid+'" expertimg="'+list[i].imageSrc+'" expertname="'+list[i].nickname+'" expertintro="'+list[i].content+'"><span class="head_cap"><img src="'+list[i].imageSrc+'"></span></a><div class="cap_cont jumpexpert" expertid="'+list[i].expertid+'"><em>'+list[i].nickname+'</em><mark>专家</mark><span class="more_icon">更多</span></div></section><section class="ball_detail"><div class="ball_time"><small>'+weekday[d]+'</small><p>'+list[i].begin_match_time.substring(5,16)+'</p></div><p class="span_contr"><strong>'+list[i].title+'</strong>';
                             if(list[i].is_hit){
                                 recommendhtml+='</p><p class="ball_case txt_contr2"><span>类型：<a>'+typeName[type]+'</a></span></p><p class="ball_case txt_contr2">推荐语:<a>'+list[i].content+'</a></p><span class="tag">'+list[i].is_hit+'</span></section></dd>';
                             }else{
@@ -92,26 +92,24 @@ $(function (){
                             
                         }else{
                             var price=list[i].price;
-                            recommendhtml+='<dd expertid="'+list[i].expertid+'" recommendid="'+list[i].recommendid+'" title="'+list[i].title+'" price="'+price+'"><section class="expert_case"><a class="gotoexpert" expertid="'+list[i].expertid+'" expertimg="'+list[i].imageSrc+'" expertname="'+list[i].nickname+'" expertintro="'+list[i].content+'"><span class="head_cap"><img src="'+list[i].imageSrc+'"></span></a><div class="cap_cont jumpexpert" expertid="'+list[i].expertid+'"><em>'+list[i].nickname+'</em><mark>高级专家</mark></div></section><section class="ball_detail"><div class="ball_time"><small>'+weekday[d]+'</small><p>'+list[i].begin_match_time.substring(5,16)+'</p></div><p class="span_contr"><strong>'+list[i].title+'</strong><span class="star_box" style="position: absolute;right:10px">￥<em>'+price+'</em></span></p><p class="ball_case txt_contr2"><span>类型：<a>'+typeName[type]+'</a></span></p><p class="ball_case txt_contr2">推荐语:<a>'+list[i].content+'</a></p><span class="tag">购买</span></section></dd>';
+                            recommendhtml+='<dd expertid="'+list[i].expertid+'" recommendid="'+list[i].recommendid+'" title="'+list[i].title+'" price="'+price+'" type="'+type+'"><section class="expert_case"><a class="gotoexpert" expertid="'+list[i].expertid+'" expertimg="'+list[i].imageSrc+'" expertname="'+list[i].nickname+'" expertintro="'+list[i].content+'"><span class="head_cap"><img src="'+list[i].imageSrc+'"></span></a><div class="cap_cont jumpexpert" expertid="'+list[i].expertid+'"><em>'+list[i].nickname+'</em><mark>专家</mark><span class="more_icon">更多</span></div></section><section class="ball_detail"><div class="ball_time"><small>'+weekday[d]+'</small><p>'+list[i].begin_match_time.substring(5,16)+'</p></div><p class="span_contr"><strong>'+list[i].title+'</strong><span class="star_box" style="position: absolute;right:10px">￥<em>'+price+'</em></span></p><p class="ball_case txt_contr2"><span>类型：<a>'+typeName[type]+'</a></span></p><p class="ball_case txt_contr2">推荐语:<a>'+list[i].content+'</a></p><span class="tag">购买</span></section></dd>';
                         }
                        
                     }
                     $("#recommendlist").empty().html(recommendhtml).delegate("dd","click",function (){  
-                            /*if(localStorage.getItem("userLogin") != "login"){
-                                location.href="/login.html";
-                                return false;
-                            }*/
                             var type=$(this).attr("type");
+                            var jump=$(this).attr("jump");
                             var recommendid=$(this).attr("recommendid");
-                            if(type !== undefined){
-                                var nickname=$(this).find(".gotoexpert").attr("expertname");
-                                var picture=$(this).find(".gotoexpert").attr("expertimg");
+                            var nickname=$(this).find(".gotoexpert").attr("expertname");
+                            var picture=$(this).find(".gotoexpert").attr("expertimg");
+                            if(jump !== undefined){
                                 location.href="/plan.html?type="+type+"&recommendid="+recommendid+"&nickname="+nickname+"&picture="+picture;
                                 return false;
                             }
                             var backTitle=$(this).attr("title");
                             var money=$(this).attr("price");
-                            weChatPay(backTitle,recommendid,money,encodeURIComponent(location.href));
+                            var weChatcallback="http://h5.wingoalclub.com/plan.html?type="+type+"replacerecommendid="+recommendid+"replacenickname="+nickname+"replacepicture="+picture;
+                            weChatPay(backTitle,recommendid,money,encodeURIComponent(weChatcallback));
                         }).delegate(".gotoexpert","click",function (event){
                             event.stopPropagation();
                             var nickname=$(this).attr("expertname");
@@ -123,6 +121,9 @@ $(function (){
                             localStorage.setItem("expertIntro",intro);
                             location.href='/expert.html?expertid='+$(this).attr("expertid");
 
+                        }).delegate(".jumpexpert","click",function (event){
+                            event.stopPropagation();
+                            $(this).siblings(".gotoexpert").click();
                         });
                 }
             }
@@ -130,9 +131,9 @@ $(function (){
     }
     
     
-    $(window).delegate(".jumpexpert","click",function (event){
+    /*$(window).delegate(".jumpexpert","click",function (event){
         event.stopPropagation();
         jumpExpert($(this));
         return false;
-    });
+    });*/
 });
